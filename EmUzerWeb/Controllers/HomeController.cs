6b00cd2f0ea6a2055d78fb7079d646cefb3d7be8
Hexanix <damian.d@abv.bg>
2017-04-29 23:31:58 +0300

    Add project files.

---
+++ EmUzerWeb/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Web.Mvc;
+using System.Web;
+using EmUzerWeb.Models;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+
+namespace EmUzerWeb.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        
+        private static SpotifyWebAPI _spotify = new SpotifyWebAPI();
+        SpotifyAccount user = new SpotifyAccount();
+
+
+        public ActionResult Index()
+        {
+            
+            
+            user.SetAccountProperties();
+            user.ConnectToSpotifyAsync(_spotify);
+            return View(user);
+        }
+
+        public ActionResult Scanner()
+        {
+
+            foreach (var item in _spotify.GetSavedTracks().Items)
+            {
+                user.outputPlaylst.Add(item.Track);
+            }
+
+
+
+            return View(user);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ EmUzerWeb/Models/SpotifyAccount.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Newtonsoft.Json.Linq;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using System.Threading.Tasks;
+
+namespace EmUzerWeb.Models
+{
+    public class SpotifyAccount
+    {
+        private static SpotifyWebAPI _spotify;
+
+        
+
+        public int UserID { get; set; }
+
+        public string AccessToken { get; set; }
+
+        public string Username { get; set; }
+
+        public string ModelError { get; set; }
+
+        public string currentEmotion { get; set; }
+
+
+        public List<SavedTrack> savedTracks { get; set; }
+
+        public List<FullTrack> outputPlaylst { get; set; }
+
+
+
+        public async Task<SpotifyWebAPI> ConnectToSpotifyAsync(SpotifyWebAPI _spotify)
+        {
+            WebAPIFactory webApiFactory = new WebAPIFactory(
+                "http://localhost",
+                 44333,
+                "509976e01ef2432c9135a8dd26085d9d",
+                Scope.PlaylistModifyPrivate | Scope.PlaylistModifyPublic | Scope.Streaming | Scope.UserFollowModify |
+                Scope.UserFollowRead | Scope.UserLibraryRead | Scope.UserReadPrivate | Scope.UserTopRead,
+                TimeSpan.FromSeconds(20));
+
+            try
+            {
+                //This will open the user's browser and returns once
+                //the user is authorized.
+                _spotify = await webApiFactory.GetWebApi();
+            }
+            catch (Exception ex)
+            {
+                ModelError = ex.Message;
+            }
+
+            if (_spotify == null)
+            {
+                throw new NullReferenceException();
+            }
+            else
+            {
+                return await Task.FromResult<SpotifyWebAPI>(_spotify);
+            }
+        }
+
+        public async void SetAccountProperties()
+        {
+            var AuthResult = await ConnectToSpotifyAsync(_spotify);
+            this.AccessToken = AuthResult.AccessToken;
+        }
+
+    }
+}
